f01ab8bfeb7c50f110dc8a2794187a1feeac0f18
janaledencan <janaledencan@gmail.com>
2021-10-31 22:22:11 +0100

    Setters, getters and constructors in Weather.cs

---
+++ DZ1/Homework1/classLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace classLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DZ1/Homework1/classLibrary/Weather.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classLibrary
+{
+    class Weather
+    {
+        private double temperature;
+        private double windSpeed;
+        private double humidity;
+
+        public double GetTemperature() { return temperature; }
+        public double GetWindSpeed() { return windSpeed; }
+        public double GetHumidity() { return humidity; }
+
+        public void SetTemperature(double temperature) { this.temperature = temperature; }
+        public void SetWindSpeed(double windSpeed) { this.windSpeed = windSpeed; }
+        public void SetHumidity(double humidity) { this.humidity = humidity; }
+
+
+        public Weather()
+        {
+            this.temperature = 21.0;
+            this.windSpeed = 0;
+            this.humidity = 2;
+        }
+
+        public Weather(double temperature, double humidity, double windSpeed)
+        {
+            this.temperature = temperature;
+            this.windSpeed = windSpeed;
+            this.humidity = humidity;
+        }
+
+
+
+    }
+}
