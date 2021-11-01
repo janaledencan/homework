08a07659d9102342418a7b3c5332888c8882b61a
janaledencan <janaledencan@gmail.com>
2021-11-01 12:30:40 +0100

    Find weather with largest wind chill

--- DZ1/Homework1/UI_1/Program.cs
+++ DZ1/Homework1/UI_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using classLibrary;
 
 namespace UI_1
 {
@@ -6,7 +7,50 @@ namespace UI_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Weather current = new Weather();
+            current.SetTemperature(24.12);
+            current.SetWindSpeed(3.5);
+            current.SetHumidity(0.56);
+            Console.WriteLine("Weather info:\n"
+                + "\ttemperature: " + current.GetTemperature() + "\n"
+                + "\thumidity: " + current.GetHumidity() + "\n"
+                + "\twind speed: " + current.GetWindSpeed() + "\n");
+            Console.WriteLine("Feels like: " + current.CalculateFeelsLikeTemperature());
+
+            Console.WriteLine("Finding weather info with largest windchill!");
+            const int Count = 5;
+            double[] temperatures = new double[Count] { 8.33, -1.45, 5.00, 12.37, 7.67 };
+            double[] humidities = new double[Count] { 0.3790, 0.4555, 0.743, 0.3750, 0.6612 };
+            double[] windSpeeds = new double[Count] { 4.81, 1.5, 5.7, 4.9, 1.2 };
+
+            Weather[] weathers = new Weather[Count];
+            for (int i = 0; i < weathers.Length; i++)
+            {
+                weathers[i] = new Weather(temperatures[i], humidities[i], windSpeeds[i]);
+                Console.WriteLine("Windchill for weathers[" + i + "] is: " + weathers[i].CalculateWindChill());
+            }
+            Weather largestWindchill = FindWeatherWithLargestWindchill(weathers);
+            Console.WriteLine(
+                "Weather info:" + largestWindchill.GetTemperature() + ", " +
+                largestWindchill.GetHumidity() + ", " + largestWindchill.GetWindSpeed()
+            );
+
+
+           static Weather FindWeatherWithLargestWindchill(Weather[] weathers)
+            {
+                int i, indx = 0;
+                double highestWindChill = weathers[0].CalculateWindChill();
+
+                for(i=1; i<weathers.Length; i++)
+                {
+                    if(weathers[i].CalculateWindChill()> highestWindChill)
+                    {
+                        highestWindChill = weathers[i].CalculateWindChill();
+                        indx = i;
+                    }
+                }
+                return (weathers[indx]);
+            }
         }
     }
 }
