69eacf45f3945ba239a6e2de21e3250f223d6f53
janaledencan <janaledencan@gmail.com>
2022-01-28 23:50:38 +0100

    WeatherApp

---
+++ DZ5/Homework5/ForecastUI/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+
+namespace ForecastUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        const string APP_ID = "518a19df22eb5a31043e51c28c80db40";
+        void GetWeatherForecast()
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}&units=metric", SearchCity.Text, APP_ID);
+
+                var json = web.DownloadString(url);
+
+                var result = JsonConvert.DeserializeObject<WeatherInformations.root>(json);
+
+                WeatherInformations.root output = result;
+
+                CityName.Text = output.name;
+                Country.Text = output.sys.country;
+
+                WeatherMessage.Text = output.weather[0].description;
+                Temperature.Text = output.main.temp.ToString() + "\u00B0C";
+                Wind.Text = "Wind: " + output.wind.speed.ToString() + " km/h";
+                Humidity.Text = "Humidity: " + output.main.humidity.ToString() + "%";
+
+
+                ShowWeatherImage(output.weather[0].id);
+            }
+        }
+
+        private void ShowWeatherImage(int weatherId)
+        {
+            BitmapImage image = new BitmapImage(new Uri("../../../Images/thunderstorm.pg", UriKind.Relative));
+            if (weatherId >= 200 && weatherId <= 232)
+            {
+                image = new BitmapImage(new Uri("../../../Images/thunderstorm.png", UriKind.Relative));
+            }
+            else if (weatherId >= 300 && weatherId <= 321)
+            {
+                image = new BitmapImage(new Uri("../../../Images/raindrops.png", UriKind.Relative));
+            }
+            else if (weatherId >= 500 && weatherId <= 531)
+            {
+                image = new BitmapImage(new Uri("../../../Images/rain.png", UriKind.Relative));
+            }
+            else if (weatherId >= 600 && weatherId <= 622)
+            {
+                image = new BitmapImage(new Uri("../../../Images/snow.png", UriKind.Relative));
+            }
+            else if (weatherId >= 700 && weatherId <= 781)
+            {
+                image = new BitmapImage(new Uri("../../../Images/refresh.png", UriKind.Relative));
+            }
+            else if (weatherId == 800)
+            {
+                image = new BitmapImage(new Uri("../../../Images/clear_night.png", UriKind.Relative));
+            }
+            else if (weatherId >= 801 && weatherId <= 804)
+            {
+                image = new BitmapImage(new Uri("../../../Images/clouds.png", UriKind.Relative));
+            }
+            WeatherFoto.Source = image;
+        }
+
+        private void SearchTask_Click(object sender, RoutedEventArgs e)
+        {
+            GetWeatherForecast();
+        }
+    }
+}
+
+       
+    
---
+++ DZ5/Homework5/ForecastUI/WeatherInformations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForecastUI
+{
+    class WeatherInformations
+    {
+
+        public class coord
+        {
+            public double lon { get; set; }
+            public double lat { get; set; }
+        }
+
+        public class weather
+        {
+            public int id { get; set; }
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+        }
+
+        public class main
+        {
+            public double temp { get; set; }
+           
+            public double FeelsLike { get; set; }
+            public int humidity { get; set; }
+        }
+
+        public class wind
+        {
+            public double speed { get; set; }
+        }
+
+        public class sys
+        {
+            public string country { get; set; }
+
+        }
+        public class root
+        {
+            public string name { get; set; }
+            public sys sys { get; set; }
+            public double dt { get; set; }
+            public wind wind { get; set; }
+            public coord coord { get; set; }
+            public List<weather> weather { get; set; }
+            public main main { get; set; }
+
+        }
+    }
+}
